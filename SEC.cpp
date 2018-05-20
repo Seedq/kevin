0377b36df50f96557ee9e8f8ade4d949effb1db5
kevin <seedq181@outlook.com>
2018-05-20 12:08:27 +0800

    s

--- SEC.cpp
+++ SEC.cpp
@@ -1,11 +1,32 @@
+#include <iostream>
 #include <stdio.h>
-
-int main()
+#include <float.h>
+using namespace std;
+int main(void)
 {
-    printf("Hello world!");
+    const int secfen (60) ;
+    int sec, min, secleft;
+    printf("This is a sec to min:sec program\n");
+    printf("You input a sec-number and the program will reguest you"
+           " a min-sec number!\n");
+    scanf_s("%d", &sec);
+    while (sec > 0)
+    {
+        min = sec / secfen;
+        secleft = sec % secfen;
+        printf("%d\n", secleft);
+        if (secleft == 0)
+        {
+            printf("your number is %dmingute\n", min);
+            printf("input your next number!\n");
+        }
+        else
+        {
+            printf("your number is %dmingute %dsec!\n", min, secleft);
+            printf("input your next number!\n");
+        }
+        scanf_s("%d", &sec);
+    }
 
-
-
-
-    return 0;
+    printf("done\n");
 }
