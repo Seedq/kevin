a9fafc67ee9261f12737bd47a1a19f7a71f739f3
kevin <seedq181@outlook.com>
2018-05-21 21:39:42 +0800

    dep

---
+++ depter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+//温度转换程序
+using namespace std;
+int tempers(double t)
+{
+
+    double kelvin, celslus;
+    celslus = 1.8 * t + 32.00;
+    kelvin = celslus + 273.16;
+    printf("%.2lf Fis %.2lf C and %.2lf K", t, celslus, kelvin);
+    return 0;
+}
+int main()
+{
+    double t;
+    printf(" is a T conversion program!\n ");
+    printf("input a Fahrenhelt T.\n");
+    scanf_s("%lf", &t);
+    tempers(t);
+
+    return 0;
+}
